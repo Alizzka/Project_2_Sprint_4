69842ad2ad7fca04a90aa31cabb33ab0ff53116e
Anastasia Semisorova <semisorova29@mail.ru>
2024-09-15 17:02:26 +0300

    new code

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -1,7 +1,7 @@
 //Выпадающий список в разделе «Вопросы о важном». Нужно проверить: когда нажимаешь на стрелочку,
 // открывается соответствующий текст.
-
 package pageobjects;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -10,9 +10,10 @@ import java.time.Duration;
 import java.util.HashMap;
 
 public class MainPage {
+
     private final WebDriver driver;
 
-    //Локаторы вопросов c 1 - 8 из раздела "Вопросы о важном":
+    // Локаторы вопросов с 1 - 8 из раздела "Вопросы о важном":
     public final static By Question_1 = By.id("accordion__heading-0");
     public final static By Question_2 = By.id("accordion__heading-1");
     public final static By Question_3 = By.id("accordion__heading-2");
@@ -22,7 +23,7 @@ public class MainPage {
     public final static By Question_7 = By.id("accordion__heading-6");
     public final static By Question_8 = By.id("accordion__heading-7");
 
-    //Локаторы ответов с 1 -8 на вопросы из раздела "Вопросы о важном":
+    // Локаторы ответов с 1 - 8 на вопросы из раздела "Вопросы о важном":
     private final static By Answer_1 = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[2]/p");
     private final static By Answer_2 = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[2]/div[2]/p");
     private final static By Answer_3 = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[3]/div[2]/p");
@@ -32,8 +33,7 @@ public class MainPage {
     private final static By Answer_7 = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[7]/div[2]/p");
     private final static By Answer_8 = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[8]/div[2]/p");
 
-
-    //Ключ:значение (вопрос:ответ):
+    // Ключ: значение (вопрос: ответ):
     static HashMap<By, By> map = new HashMap<>();
     static {
         map.put(Question_1, Answer_1);
@@ -50,77 +50,32 @@ public class MainPage {
         this.driver = driver;
     }
 
-    //Скролим главную страницу до первого вопроса:
+    // Скролим главную страницу до первого вопроса:
     public void scrollMainPage() {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();",
-                driver.findElement(By.id("accordion__heading-0")));
+                driver.findElement(Question_1));
     }
 
-    //Открыть элемент списка вопросов, ждем появления текста с ответом:
-    public String getAnswer(By id) {
-        driver.findElement(id).click();
-        new WebDriverWait(driver, Duration.ofSeconds(15)).until(driver -> (driver.findElement(map.get(id)).getText() != null
-                && !driver.findElement(map.get(id)).getText().isEmpty()));
-        return driver.findElement(map.get(id)).getText();
+    // Открыть элемент списка вопросов, ждем появления текста с ответом:
+    public String getAnswer(By questionLocator) {
+        driver.findElement(questionLocator).click();
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(driver ->
+                driver.findElement(map.get(questionLocator)).getText() != null
+                        && !driver.findElement(map.get(questionLocator)).getText().isEmpty());
+        return driver.findElement(map.get(questionLocator)).getText();
+    }
+
+    // Новый метод для получения ответа на вопрос по тексту:
+    public String getAnswerForQuestion(String question) {
+        // Логика получения ответа на вопрос
+        // Предполагаем, что вопрос будет использоваться для получения локатора
+        // В реальной реализации можно использовать поиск в мапе или другой подход
+        for (By questionLocator : map.keySet()) {
+            if (driver.findElement(questionLocator).getText().contains(question)) {
+                return getAnswer(questionLocator);
+            }
+        }
+        return "Ответ не найден";
     }
 }
 
-
-//говнокод
-/*package pageobjects;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
-
-public class MainPage {
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    // Заголовок страницы
-    private By pageTitle = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[4]");
-
-    // Кнопка "Заказать" вверху страницы
-    private By orderButtonTop = By.xpath(".//button[text()='Заказать'][1]");
-
-    // Кнопка "Заказать" внизу страницы
-    private By orderButtonBottom = By.xpath(".//button[text()='Заказать'][2]");
-
-    // Вопросы и ответы в разделе "Вопросы о важном"
-    private By faqSection = By.xpath("//*[@id=\"root\"]/div/div/div[5]/div[1]");
-
-    public MainPage(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 секунд ожидания
-    }
-
-    // Нажать на кнопку "Заказать" вверху страницы
-    public void clickOrderButtonTop() {
-        driver.findElement(orderButtonTop).click();
-    }
-
-    // Нажать на кнопку "Заказать" внизу страницы
-    public void clickOrderButtonBottom() {
-        driver.findElement(orderButtonBottom).click();
-    }
-
-    // Открыть выпадающий список FAQ
-    public void openFaqQuestion(int index) {
-        // Ожидаем, что раздел FAQ будет доступен
-        WebElement faqSectionElement = wait.until(ExpectedConditions.visibilityOfElementLocated(faqSection));
-
-        // Находим все вопросы FAQ
-        List<WebElement> questions = faqSectionElement.findElements(By.xpath(".//button")); // Предполагается, что вопросы FAQ - это кнопки
-
-        if (index < questions.size()) {
-            questions.get(index).click();
-        } else {
-            throw new IndexOutOfBoundsException("FAQ question index out of bounds.");
-        }
-    }
-}*/
--- src/test/java/FaqTest.java
+++ src/test/java/FaqTest.java
@@ -1,19 +1,46 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
 import pageobjects.MainPage;
 
 import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class FaqTest {
 
     private WebDriver driver;
-    private MainPage generalPage;
+    private MainPage mainPage;
+    private final String question;
+    private final String expectedAnswer;
+
+    public FaqTest(String question, String expectedAnswer) {
+        this.question = question;
+        this.expectedAnswer = expectedAnswer;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."}
+        });
+    }
 
     @Before
     public void setUp() {
@@ -22,73 +49,18 @@ public class FaqTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        generalPage = new MainPage(driver);  // Инициализация MainPage
+        mainPage = PageFactory.initElements(driver, MainPage.class);
     }
 
     @Test
-    public void checkFirstQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-                generalPage.getAnswer(MainPage.Question_1));
-    }
-
-    @Test
-    public void checkSecondQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, " +
-                        "можете просто сделать несколько заказов — один за другим.",
-                generalPage.getAnswer(MainPage.Question_2));
-    }
-
-    @Test
-    public void checkThirdQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. " +
-                        "Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. " +
-                        "Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
-                generalPage.getAnswer(MainPage.Question_3));
-    }
-
-    @Test
-    public void checkFourthQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
-                generalPage.getAnswer(MainPage.Question_4));
-    }
-
-    @Test
-    public void checkFifthQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Пока что нет! Но если что-то срочное — всегда можно позвонить " +
-                        "в поддержку по красивому номеру 1010.",
-                generalPage.getAnswer(MainPage.Question_5));
-    }
-
-    @Test
-    public void checkSixthQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — " +
-                        "даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
-                generalPage.getAnswer(MainPage.Question_6));
-    }
-
-    @Test
-    public void checkSeventhQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Да, пока самокат не привезли. Штрафа не будет, " +
-                        "объяснительной записки тоже не попросим. Все же свои.",
-                generalPage.getAnswer(MainPage.Question_7));
-    }
-
-    @Test
-    public void checkEighthQuestion() {
-        generalPage.scrollMainPage();
-        assertEquals("Да, обязательно. Всем самокатов! И Москве, и Московской области.",
-                generalPage.getAnswer(MainPage.Question_8));
+    public void checkQuestion() {
+        mainPage.scrollMainPage(); // Скролим страницу, чтобы вопросы были видны
+        String actualAnswer = mainPage.getAnswerForQuestion(question);
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
     @After
-    public void teardown() {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
@@ -96,55 +68,5 @@ public class FaqTest {
 }
 
 
-//говнокод
-/*package tests.order;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import pageobjects.MainPage;
-
-import java.time.Duration;
-
-import static org.junit.Assert.assertTrue;
-
-public class FaqTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private MainPage mainPage;
-
-    @Before
-    public void setUp() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Увеличенное время ожидания
-        mainPage = new MainPage(driver);
-        driver.get("https://qa-scooter.praktikum-services.ru/");
-    }
-
-    @Test
-    public void checkFaqDropdown() {
-        // Ожидание видимости заголовка страницы
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[4]")));
-
-        // Нажать на первый вопрос в FAQ
-        mainPage.openFaqQuestion(0);
-
-        // Добавьте проверки видимости текста, если нужно
-        // Например:
-        WebElement answer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[5]/div[1]")));
-        assertTrue(answer.isDisplayed());
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
-}*/
 
