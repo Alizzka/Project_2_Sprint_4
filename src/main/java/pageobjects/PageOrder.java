82de317266799734589b27af47f8373efdb1b158
Anastasia Semisorova <semisorova29@mail.ru>
2024-09-15 18:57:33 +0300

    new code

--- src/main/java/pageobjects/PageOrder.java
+++ src/main/java/pageobjects/PageOrder.java
@@ -6,98 +6,97 @@
 - Проверить, что появилось всплывающее окно с сообщением об успешном создании заказа.*/
 
 package pageobjects;
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.Keys;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-        import java.util.List;
+import java.util.List;
 
 public class PageOrder {
     private final WebDriver driver;
 
-    //Локаторы для первой страницы
+    // Локаторы для баннера куки
+    private static final By CookieBannerButton = By.className("App_CookieButton__3cvqF");
 
-    //Локатор для кнопки "Заказать" в верхней части страницы:
-    private static final By OrderButtonHeader = By.xpath("/html/body/div/div/div[1]/div[1]/div[2]/button[1]");
-    //Локатор для кнопки "Заказать" в нижней части страницы:
+    // Локаторы для первой страницы
+    private static final By OrderButtonHeader = By.xpath("//*[@id='root']/div/div[1]/div[1]/div[2]/button[1]");
     private static final By OrderButtonDown = By.className("Button_UltraBig__UU3Lp");
-    //Локатор для поля "Имя":
-    private static final By NameButton = By.xpath("/html/body/div/div/div[2]/div[2]/div[1]/input");
-    //Локатор для поля "Фамилия":
-    private static final By SurnameButton = By.xpath("//div[2]/input");
-    //Локатор для поля "Адрес: куда привезти заказ":
-    private static final By AddressButton = By.xpath("//div[3]/input");
-    //Локатор для поля "Станция метро":
-    private static final By MetroButton = By.xpath("/html/body/div/div/div[2]/div[2]/div[4]/div/div/input");
-    //Локатор для поля "Телефон: на него позвонит курьер":
-    private static final By TelButton = By.xpath("//div[5]/input");
-    //Локатор для кнопки "Далее":
-    private static final By NextButton = By.className("Button_Middle__1CSJM");
+    private static final By NameButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[1]/input");
+    private static final By SurnameButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[2]/input");
+    private static final By AddressButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[3]/input");
+    private static final By MetroButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[4]/div/div/input");
+    private static final By TelButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[5]/input");
+    private static final By NextButton = By.xpath("//*[@id='root']/div/div[2]/div[3]/button");
 
-    //Локаторы для второй страницы
-
-    //Локатор для поля "*Когда привезти самокат":
-    private static final By DateButton = By.className("Input_Responsible__1jDKN");
-    //Локатор для поля "*Срок аренды":
-    private static final By PeriodButton = By.className("Dropdown-placeholder");
-    //Локатор для поля "Цвет самоката", с выбором цвета черный жемчуг:
-    private static final By BlackButton = By.className("Checkbox_Input__14A2w");
-    //Локатор для поля "Цвет самоката", с выбором цвета серая безысходность:
-    private static final By GreyButton = By.id("grey");
-    //Локатор для поля "Комментарий для курьера":
-    private static final By CommentButton = By.xpath("/html/body/div/div/div[2]/div[2]/div[4]/input");
-    //Локатор для кнопки "Заказать":
-    private static final By SubmitButton = By.xpath("/html/body/div[1]/div/div[2]/div[3]/button[2]");
-    //Локатор для попапа "Хотите оформить заказ?":
-    private static final By PopupButton = By.xpath("/html/body/div/div/div[2]/div[5]/div[1]");
-    //Локатор для кнопки "Да":
-    private static final By YesButton = By.xpath("/html/body/div/div/div[2]/div[5]/div[2]/button[2]");
+    // Локаторы для второй страницы
+    private static final By DateButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[1]/div/div/input");
+    private static final By PeriodButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[2]/div/div");
+    private static final By BlackButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[3]/label[1]/input");
+    private static final By GreyButton = By.xpath("//*[@id='grey']");
+    private static final By CommentButton = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[4]/input");
+    private static final By SubmitButton = By.xpath("//*[@id='root']/div/div[2]/div[3]/button[2]");
+    private static final By PopapButton = By.xpath("//*[@id='root']/div/div[2]/div[5]/div[1]");
+    private static final By YesButton = By.xpath("//*[@id='root']/div/div[2]/div[5]/div[2]/button[2]");
 
     public PageOrder(WebDriver driver) {
         this.driver = driver;
     }
 
-    //Заполнение первой страницы заказа:
-    public void enterDataFirstPageOrder(String name, String surname, String address, int metro, String phoneNumber) {
-        driver.findElement(OrderButtonHeader).click();
-        driver.findElement(NameButton).sendKeys(name);
-        driver.findElement(SurnameButton).sendKeys(surname);
-        driver.findElement(AddressButton).sendKeys(address);
-        driver.findElement(MetroButton).click();
-        By allMetroStation = By.xpath("/html/body/div/div/div[2]/div[2]/div[4]/div/div[2]/ul/li");
-        //Список станций метро:
-        List<WebElement> elements = driver.findElements(allMetroStation);
-        elements.get(metro - 1).click();
-        driver.findElement(TelButton).sendKeys(phoneNumber);
-        driver.findElement(NextButton).click();
+    // Метод для закрытия баннера куки
+    public void closeCookieBanner() {
+        driver.findElement(CookieBannerButton).click();
     }
 
-    //Заполнение второй страницы заказа:
+    // Заполнение первой страницы заказа
+    public void enterDataFirstPageOrder(String name, String surname, String address, int metro, String phoneNumber) {
+        driver.findElement(OrderButtonHeader).click();  // Нажатие на кнопку "Заказать"
+        driver.findElement(NameButton).sendKeys(name);  // Заполнение поля "Имя"
+        driver.findElement(SurnameButton).sendKeys(surname);  // Заполнение поля "Фамилия"
+        driver.findElement(AddressButton).sendKeys(address);  // Заполнение поля "Адрес"
+        driver.findElement(MetroButton).click();  // Нажатие на поле "Станция метро"
+
+        // Выбор станции метро из списка
+        By allMetroStation = By.xpath("//*[@id='root']/div/div[2]/div[2]/div[4]/div/div[2]/ul/li");
+        List<WebElement> elements = driver.findElements(allMetroStation);
+        elements.get(metro - 1).click();  // Выбор станции метро по индексу
+
+        driver.findElement(TelButton).sendKeys(phoneNumber);  // Заполнение поля "Телефон"
+        driver.findElement(NextButton).click();  // Нажатие на кнопку "Далее"
+    }
+
+    // Заполнение второй страницы заказа
     public void enterDataSecondPageOrder(String color, String date, int rentalDays, String comment) {
+        // Выбор цвета самоката
         if ("черный".equals(color)) {
             driver.findElement(BlackButton).click();
-        }
-        if ("серый".equals(color)) {
+        } else if ("серый".equals(color)) {
             driver.findElement(GreyButton).click();
         }
-        //Выбор даты:
+
+        // Выбор даты
         driver.findElement(DateButton).sendKeys(date);
         driver.findElement(DateButton).sendKeys(Keys.RETURN);
+
+        // Выбор срока аренды
         driver.findElement(PeriodButton).click();
         By allDayForOrder = By.className("Dropdown-option");
-        //Список из 7 суток аренды:
         List<WebElement> elements = driver.findElements(allDayForOrder);
-        elements.get(rentalDays - 1).click();
-        driver.findElement(CommentButton).sendKeys(comment);
-        driver.findElement(SubmitButton).click();
-        driver.findElement(YesButton).click();
+        elements.get(rentalDays - 1).click();  // Выбор срока аренды
+
+        driver.findElement(CommentButton).sendKeys(comment);  // Заполнение поля "Комментарий"
+        driver.findElement(SubmitButton).click();  // Нажатие на кнопку "Заказать"
+        driver.findElement(YesButton).click();  // Подтверждение заказа в попапе
     }
 
-    //Попап подтверждения оформления заказа:
+    // Попап подтверждения оформления заказа
     public boolean successfullyText() {
-        String successfullOrder = driver.findElement(PopupButton).getText();
+        String successfullOrder = driver.findElement(PopapButton).getText();
         return successfullOrder.contains("Заказ оформлен");
     }
 }
+
+
+
+
 
--- src/test/java/OrderScooterTest.java
+++ src/test/java/OrderScooterTest.java
@@ -1,19 +1,44 @@
-import org.junit.Assert;
-import pageobjects.PageOrder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pageobjects.PageOrder;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
 public class OrderScooterTest {
 
     private WebDriver driver;
+    private PageOrder pageOrder;
 
-    private PageOrder generalPage; // Инициализация PageOrder ниже!
+    // Параметры теста
+    @Parameterized.Parameter(0)
+    public String name;
+    @Parameterized.Parameter(1)
+    public String surname;
+    @Parameterized.Parameter(2)
+    public String address;
+    @Parameterized.Parameter(3)
+    public int metro;
+    @Parameterized.Parameter(4)
+    public String phoneNumber;
+    @Parameterized.Parameter(5)
+    public String color;
+    @Parameterized.Parameter(6)
+    public String date;
+    @Parameterized.Parameter(7)
+    public int rentalDays;
+    @Parameterized.Parameter(8)
+    public String comment;
 
     @Before
     public void setUp() {
@@ -22,43 +47,37 @@ public class OrderScooterTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        generalPage = new PageOrder(driver);  // Инициализация PageOrder
+        pageOrder = new PageOrder(driver);
+
+        // Закрытие баннера с cookie, если он есть
+        pageOrder.closeCookieBanner();
     }
 
     @Test
-    public void testOrderScooterOne() {
-        enterDataFirstPageOrder("Галина", "Петровна", "Москва", 3, "+79667653344");
-        enterDataSecondPageOrder("черный", "16.09.2024", 7, "Позвоните за 30 минут");
-        Assert.assertTrue(generalPage.successfullyText());
-    }
-
-    private void enterDataSecondPageOrder(String цвет, String дата, int срок, String комментарий) {
-        // Реализовать ввод данных на второй странице. Для теста Выше!
-    }
-
-    private void enterDataFirstPageOrder(String имя, String фамилия, String адрес, int номерКвартиры, String телефон) {
-        // Реализовать ввод данных на первой странице. Для теста ниже!
-    }
-
-    @Test
-    public void testOrderScooterTwo() {
-        enterDataFirstPageOrder("Роман", "Максимов", "Москва", 15, "+79541112233");
-        enterDataSecondPageOrder("серый", "18.10.2024", 5, "");
-        Assert.assertTrue(generalPage.successfullyText());
-    }
-
-    @Test
-    public void testOrderScooterThree() {
-        enterDataFirstPageOrder("Петр", "Первый", "Санкт-Петербург", 10, "+75126778894");
-        enterDataSecondPageOrder("черный", "27.09.2024", 3, "Оставить около двери");
-        Assert.assertTrue(generalPage.successfullyText());
+    public void testOrderScooter() {
+        pageOrder.enterDataFirstPageOrder(name, surname, address, metro, phoneNumber);
+        pageOrder.enterDataSecondPageOrder(color, date, rentalDays, comment);
+        assertTrue(pageOrder.successfullyText());
     }
 
     @After
     public void teardown() {
         driver.quit();
     }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {"Галина", "Петровна", "Москва", 3, "+79667653344", "черный", "16.09.2024", 7, "Позвоните за 30 минут"},
+                {"Роман", "Максимов", "Москва", 15, "+79541112233", "серый", "18.10.2024", 5, ""},
+                {"Петр", "Первый", "Санкт-Петербург", 10, "+75126778894", "черный", "27.09.2024", 3, "Оставить около двери"}
+        });
+    }
 }
+
+
+
+
 
 
 
